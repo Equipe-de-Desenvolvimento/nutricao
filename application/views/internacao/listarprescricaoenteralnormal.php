4bdc39204784aeb53964188e0f8cd834476967f5
sisprod <sisprod@sisprod>
2016-09-28 15:48:48 -0300

    HAMILTON

--- application/views/internacao/listarprescricaoenteralnormal.php
+++ application/views/internacao/listarprescricaoenteralnormal.php
@@ -54,6 +54,7 @@ IF ($tipo == 'ENTERALNORMAL') {
             $i = "";
             $b = 0;
             $w = 0;
+            $z = 0;
             $equipo = "";
             $verificaprimeiro = 2;
             $produto = "";
@@ -89,7 +90,7 @@ IF ($tipo == 'ENTERALNORMAL') {
                             $volume = "";
                         }
                         if ($item->frasco != 0) {
-                            $frasco = $item->frasco . "ml";
+                            $frasco = $item->frasco . " ml";
                         } else {
                             $frasco = "";
                         }
@@ -100,7 +101,7 @@ IF ($tipo == 'ENTERALNORMAL') {
 //                        $frasco = "";
                         }
                         if ($item->kcal != "") {
-                            $kcal = $item->kcal . $item->descricao;
+                            $kcal = $item->kcal . " " . $item->descricao . " ";
                         } else {
                             $kcal = "";
                         }
@@ -108,19 +109,22 @@ IF ($tipo == 'ENTERALNORMAL') {
 
 
                         if ($b == 1) {
-                            $produto = $i . "(" . $volume . $kcal . $item->nome;
+                            $produto = $i . " (" . $volume . $kcal . $item->nome;
                         } else {
                             if ($internacao_precricao_etapa_id == $item->internacao_precricao_etapa_id) {
-                                $produto = $produto . "+" . $volume . $kcal . $item->nome . ") " . $frasco;
+                                $produto = $produto . " + " . $volume . $kcal . $item->nome . ") " . $frasco;
                                 $w++;
                             } elseif($w >0) {
-                                $produto = $produto . "+" . $i . "(" . $volume . $kcal . $item->nome . ") " . $frasco;
+                                $produto = $produto . " + " . $i . " (" . $volume . $kcal . $item->nome . ") " . $frasco;
+                                $z++;
+                            }elseif($z ==0){
+                                $produto = $produto  . " + " . $i . " (" . $volume . $kcal . $item->nome . ") " . $frasco;
                             }else{
-                                $produto = $produto  . ") " . $frasco2 . "+" . $i . "(" . $volume . $kcal . $item->nome . ") " . $frasco;
+                                $produto = $produto  . ") " . $frasco2 . " + " . $i . " (" . $volume . $kcal . $item->nome . ") " . $frasco;
                             }
                         }
                         if ($item->frasco != 0) {
-                            $frasco2 = $item->frasco . "ml";
+                            $frasco2 = $item->frasco . " ml";
                         } else {
                             $frasco2 = "";
                         }
@@ -139,6 +143,7 @@ IF ($tipo == 'ENTERALNORMAL') {
                 }
                 $b = 0;
                 $w = 0;
+                $z = 0;
                 ?>
             <tr>
                 <?
@@ -155,17 +160,16 @@ IF ($tipo == 'ENTERALNORMAL') {
                     <td ><font size = -3><?= utf8_decode($hospital); ?></font></td>
                     <td ><font size = -3><?= utf8_decode($leito); ?></font></td>
                     <td ><font size = -3><?= substr($nascimento, 8, 2) . "/" . substr($nascimento, 5, 2) . "/" . substr($nascimento, 0, 4); ?></font></td>
-                    <td ><font size = -3><?= utf8_decode($paciente); ?></font></td>
-                    <td ><font size = -3><?= utf8_decode($produto) ?></font></td>
+                    <td ><font size = -1><b><?= utf8_decode($paciente); ?></b></font></td>
+                    <td ><font size = -1><b><?= utf8_decode($produto) ?></b></font></td>
                     <td ><font size = -3><?= $equipo; ?></font></td>
                     <td ><font size = -3><?= $vazao . " ml/h"; ?></font></td>
                 </tr>
                 <?
             }
             $produto = "";
-            ?>
+            $equipo = "";
 
-            <?
         }
         ?>
         <tr><th colspan="2" class="tabela_header">Total de Pacientes: <?= $totalpacientes; ?></th><th colspan="6" class="tabela_header">&nbsp;</th></tr>
